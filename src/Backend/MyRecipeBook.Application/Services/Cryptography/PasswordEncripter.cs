896740ffa37c0c60d8b094b5e14b4e88542cfe40
Welisson Arley <welissonarleyvs@gmail.com>
2024-01-01 10:37:21 +0000

    feat: adding password encripter

---
+++ src/Backend/MyRecipeBook.Application/Services/Cryptography/PasswordEncripter.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MyRecipeBook.Application.Services.Cryptography;
+
+public class PasswordEncripter
+{
+    private readonly string _additionalKey;
+
+    public PasswordEncripter(string additionalKey) => _additionalKey = additionalKey;
+
+    public string Encrypt(string password)
+    {
+        var newPassword = $"{password}{_additionalKey}";
+
+        var bytes = Encoding.UTF8.GetBytes(newPassword);
+        var hashBytes = SHA512.HashData(bytes);
+
+        return StringBytes(hashBytes);
+    }
+
+    private static string StringBytes(byte[] bytes)
+    {
+        var sb = new StringBuilder();
+        foreach (byte b in bytes)
+        {
+            var hex = b.ToString("x2");
+            sb.Append(hex);
+        }
+
+        return sb.ToString();
+    }
+}
