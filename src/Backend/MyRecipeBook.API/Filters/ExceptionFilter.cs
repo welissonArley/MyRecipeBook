d2a0a2c3fa926a515078d55b39badb754121dfbc
Welisson Arley <welissonarleyvs@gmail.com>
2024-01-01 10:39:39 +0000

    feat: adding filter exception

---
+++ src/Backend/MyRecipeBook.API/Filters/ExceptionFilter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionsBase;
+using System.Net;
+
+namespace MyRecipeBook.API.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if(context.Exception is MyRecipeBookException)
+            HandleProjectException(context);
+        else
+            ThrowUnknowException(context);
+    }
+
+    private void HandleProjectException(ExceptionContext context)
+    {
+        if(context.Exception is ErrorOnValidationException)
+        {
+            var exception = context.Exception as ErrorOnValidationException;
+
+            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            context.Result = new BadRequestObjectResult(new ResponseErrorJson(exception.ErrorMessages));
+        }
+    }
+
+    private void ThrowUnknowException(ExceptionContext context)
+    {
+        context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        context.Result = new ObjectResult(new ResponseErrorJson(ResourceMessagesException.UNKNOWN_ERROR));
+    }
+}
