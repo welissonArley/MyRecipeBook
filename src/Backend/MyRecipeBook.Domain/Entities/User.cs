97d8f658df631c3f2235f04bad51154252e9cde0
Welisson Arley <welissonarleyvs@gmail.com>
2024-01-01 10:11:46 +0000

    feat: adding user entity

---
+++ src/Backend/MyRecipeBook.Domain/Entities/User.cs
@@ -0,0 +1,7 @@
+﻿namespace MyRecipeBook.Domain.Entities;
+public class User : EntityBase
+{
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
