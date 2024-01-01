b95fde24d35b117dbf125e15499a98a407e92822
Welisson Arley <welissonarleyvs@gmail.com>
2024-01-01 09:47:07 +0000

    feat: adding response error json

---
+++ src/Shared/MyRecipeBook.Communication/Responses/ResponseErrorJson.cs
@@ -0,0 +1,15 @@
+﻿namespace MyRecipeBook.Communication.Responses;
+public class ResponseErrorJson
+{
+    public IList<string> Errors { get; set; }
+
+    public ResponseErrorJson(IList<string> errors) => Errors = errors;
+
+    public ResponseErrorJson(string error)
+    {
+        Errors = new List<string>
+        {
+            error
+        };
+    }
+}
