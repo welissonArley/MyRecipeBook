854dfbf86148025be4930bd1f7431a65c408fb35
Welisson Arley <welissonarleyvs@gmail.com>
2024-01-01 10:36:08 +0000

    feat: adding automapper profile

---
+++ src/Backend/MyRecipeBook.Application/Services/AutoMapper/AutoMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MyRecipeBook.Communication.Requests;
+
+namespace MyRecipeBook.Application.Services.AutoMapper;
+
+public class AutoMapping : Profile
+{
+    public AutoMapping()
+    {
+        RequestToDomain();
+    }
+
+    private void RequestToDomain()
+    {
+        CreateMap<RequestRegisterUserJson, Domain.Entities.User>()
+            .ForMember(dest => dest.Password, opt => opt.Ignore());
+    }
+}
