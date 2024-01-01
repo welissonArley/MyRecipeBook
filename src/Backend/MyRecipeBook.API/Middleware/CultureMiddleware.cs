22b5b2c11b9ceecf208a76e301806d9eb99c4deb
Welisson Arley <welissonarleyvs@gmail.com>
2024-01-01 10:40:39 +0000

    feat: adding culture middleware

---
+++ src/Backend/MyRecipeBook.API/Middleware/CultureMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace MyRecipeBook.API.Middleware;
+
+public class CultureMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public CultureMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        var supportedLanguages = CultureInfo.GetCultures(CultureTypes.AllCultures);
+
+        var requestedCulture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+
+        var cultureInfo = new CultureInfo("en");
+
+        if(string.IsNullOrWhiteSpace(requestedCulture) == false
+            && supportedLanguages.Any(c => c.Name.Equals(requestedCulture)))
+        {
+            cultureInfo = new CultureInfo(requestedCulture);
+        }
+
+        CultureInfo.CurrentCulture = cultureInfo;
+        CultureInfo.CurrentUICulture = cultureInfo;
+
+        await _next(context);
+    }
+}
