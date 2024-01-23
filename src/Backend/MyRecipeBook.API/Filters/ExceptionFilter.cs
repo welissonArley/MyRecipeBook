c03638072aa30c55e4ad9504ed45ed4f3c168244
Welisson Arley <welissonarleyvs@gmail.com>
2024-01-23 07:32:00 +0000

    feat: adding login exception on filter

--- src/Backend/MyRecipeBook.API/Filters/ExceptionFilter.cs
+++ src/Backend/MyRecipeBook.API/Filters/ExceptionFilter.cs
@@ -19,7 +19,12 @@ public class ExceptionFilter : IExceptionFilter
 
     private static void HandleProjectException(ExceptionContext context)
     {
-        if(context.Exception is ErrorOnValidationException)
+        if (context.Exception is InvalidLoginException)
+        {
+            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+            context.Result = new UnauthorizedObjectResult(new ResponseErrorJson(context.Exception.Message));
+        }
+        else if (context.Exception is ErrorOnValidationException)
         {
             var exception = context.Exception as ErrorOnValidationException;
 
