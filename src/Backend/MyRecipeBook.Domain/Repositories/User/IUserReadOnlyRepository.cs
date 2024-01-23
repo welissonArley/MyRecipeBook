c6be7f90f58ed27ea0fb9e7d3931282d75e9c804
Welisson Arley <welissonarleyvs@gmail.com>
2024-01-23 07:26:28 +0000

    feat: adding function to login user on repository

--- src/Backend/MyRecipeBook.Domain/Repositories/User/IUserReadOnlyRepository.cs
+++ src/Backend/MyRecipeBook.Domain/Repositories/User/IUserReadOnlyRepository.cs
@@ -2,4 +2,5 @@
 public interface IUserReadOnlyRepository
 {
     public Task<bool> ExistActiveUserWithEmail(string email);
+    public Task<Entities.User?> GetByEmailAndPassword(string email, string password);
 }
--- src/Backend/MyRecipeBook.Infrastructure/DataAccess/Repositories/UserRepository.cs
+++ src/Backend/MyRecipeBook.Infrastructure/DataAccess/Repositories/UserRepository.cs
@@ -13,4 +13,12 @@ public class UserRepository : IUserWriteOnlyRepository, IUserReadOnlyRepository
     public async Task Add(User user) => await _dbContext.Users.AddAsync(user);
 
     public async Task<bool> ExistActiveUserWithEmail(string email) => await _dbContext.Users.AnyAsync(user => user.Email.Equals(email) && user.Active);
+
+    public async Task<User?> GetByEmailAndPassword(string email, string password)
+    {
+        return await _dbContext
+            .Users
+            .AsNoTracking()
+            .FirstOrDefaultAsync(user => user.Active && user.Email.Equals(email) && user.Password.Equals(password));
+    }
 }
