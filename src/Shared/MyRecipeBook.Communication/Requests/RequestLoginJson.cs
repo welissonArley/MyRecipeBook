c5586948ecbc205e8125aea823a2c6511f7098ca
Welisson Arley <welissonarleyvs@gmail.com>
2024-01-23 07:29:23 +0000

    feat: adding request to do login

---
+++ src/Shared/MyRecipeBook.Communication/Requests/RequestLoginJson.cs
@@ -0,0 +1,7 @@
+﻿namespace MyRecipeBook.Communication.Requests;
+
+public class RequestLoginJson
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
