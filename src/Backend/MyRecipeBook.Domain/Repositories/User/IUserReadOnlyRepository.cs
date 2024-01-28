13ae1f0734710659570c09bddbed90d1fd9b0ef0
Welisson Arley <welissonarleyvs@gmail.com>
2024-01-28 17:44:45 +0000

    feat: adding user repository methods to work with authorization

--- src/Backend/MyRecipeBook.Domain/Repositories/User/IUserReadOnlyRepository.cs
+++ src/Backend/MyRecipeBook.Domain/Repositories/User/IUserReadOnlyRepository.cs
@@ -2,5 +2,7 @@
 public interface IUserReadOnlyRepository
 {
     public Task<bool> ExistActiveUserWithEmail(string email);
+    public Task<bool> ExistActiveUserWithIdentifier(Guid userIdentifier);
     public Task<Entities.User?> GetByEmailAndPassword(string email, string password);
+    public Task<Entities.User> GetByUserIdentifier(Guid userIdentifier);
 }
