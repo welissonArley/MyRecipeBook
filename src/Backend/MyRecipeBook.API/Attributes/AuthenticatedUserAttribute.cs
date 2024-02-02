157c0a84171896bc01403129e3beaba2707c880e
Welisson Arley <welissonarleyvs@gmail.com>
2024-02-02 10:03:55 +0000

    feat: adding attribute to check user authentication

---
+++ src/Backend/MyRecipeBook.API/Attributes/AuthenticatedUserAttribute.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.API.Filters;
+
+namespace MyRecipeBook.API.Attributes;
+
+public class AuthenticatedUserAttribute : TypeFilterAttribute
+{
+    public AuthenticatedUserAttribute() : base(typeof(AuthenticatedUserFilter))
+    {
+    }
+}
--- src/Shared/MyRecipeBook.Communication/Responses/ResponseErrorJson.cs
+++ src/Shared/MyRecipeBook.Communication/Responses/ResponseErrorJson.cs
@@ -5,6 +5,8 @@ public class ResponseErrorJson
 
     public ResponseErrorJson(IList<string> errors) => Errors = errors;
 
+    public bool TokenIsExpired { get; set; }
+
     public ResponseErrorJson(string error)
     {
         Errors = new List<string>
