c0a3a3c349e1aec40f5b2279a8c65c33791cdd87
Welisson Arley <welissonarleyvs@gmail.com>
2024-05-01 15:12:41 +0100

    tests: adding first part of test for recipe validator

---
+++ tests/CommonTestUtilities/Requests/RequestRecipeJsonBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Bogus;
+using MyRecipeBook.Communication.Enums;
+using MyRecipeBook.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+public class RequestRecipeJsonBuilder
+{
+    public static RequestRecipeJson Build()
+    {
+        return new Faker<RequestRecipeJson>()
+            .RuleFor(r => r.Title, f => f.Lorem.Word())
+            .RuleFor(r => r.CookingTime, f => f.PickRandom<CookingTime>())
+            .RuleFor(r => r.Difficulty, f => f.PickRandom<Difficulty>());
+    }
+}
---
+++ tests/Validators.Test/Recipe/RecipeValidatorTest.cs
@@ -0,0 +1,92 @@
+﻿using CommonTestUtilities.Requests;
+using FluentAssertions;
+using MyRecipeBook.Application.UseCases.Recipe;
+using MyRecipeBook.Exceptions;
+using Xunit;
+
+namespace Validators.Test.Recipe;
+public class RecipeValidatorTest
+{
+    [Fact]
+    public void Success()
+    {
+        var validator = new RecipeValidator();
+
+        var request = RequestRecipeJsonBuilder.Build();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Error_Invalid_Cooking_Time()
+    {
+        var validator = new RecipeValidator();
+
+        var request = RequestRecipeJsonBuilder.Build();
+        request.CookingTime = (MyRecipeBook.Communication.Enums.CookingTime?)1000;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceMessagesException.COOKING_TIME_NOT_SUPPORTED));
+    }
+
+    [Fact]
+    public void Error_Invalid_Difficulty()
+    {
+        var validator = new RecipeValidator();
+
+        var request = RequestRecipeJsonBuilder.Build();
+        request.Difficulty = (MyRecipeBook.Communication.Enums.Difficulty?)1000;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceMessagesException.DIFFICULTY_LEVEL_NOT_SUPPORTED));
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("          ")]
+    [InlineData("")]
+    public void Error_Empty_Title(string title)
+    {
+        var validator = new RecipeValidator();
+
+        var request = RequestRecipeJsonBuilder.Build();
+        request.Title = title;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceMessagesException.RECIPE_TITLE_EMPTY));
+    }
+
+    [Fact]
+    public void Success_Cooking_Time_Null()
+    {
+        var validator = new RecipeValidator();
+
+        var request = RequestRecipeJsonBuilder.Build();
+        request.CookingTime = null;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Success_Difficulty_Null()
+    {
+        var validator = new RecipeValidator();
+
+        var request = RequestRecipeJsonBuilder.Build();
+        request.Difficulty = null;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeTrue();
+    }
+}
