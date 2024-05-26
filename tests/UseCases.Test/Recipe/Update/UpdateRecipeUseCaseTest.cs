60e17b74f5a0f12e8974ab01dafe3973cfefe5ab
Welisson Arley <welissonarleyvs@gmail.com>
2024-05-26 20:38:22 +0100

    tests: unit tests for update a recipe

---
+++ tests/UseCases.Test/Recipe/Update/UpdateRecipeUseCaseTest.cs
@@ -0,0 +1,72 @@
+﻿using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Mapper;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using MyRecipeBook.Application.UseCases.Recipe.Update;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionsBase;
+using Xunit;
+
+namespace UseCases.Test.Recipe.Update;
+public class UpdateRecipeUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        (var user, _) = UserBuilder.Build();
+        var recipe = RecipeBuilder.Build(user);
+        var request = RequestRecipeJsonBuilder.Build();
+
+        var useCase = CreateUseCase(user, recipe);
+
+        Func<Task> act = async () => await useCase.Execute(recipe.Id, request);
+
+        await act.Should().NotThrowAsync();
+    }
+
+    [Fact]
+    public async Task Error_Recipe_NotFound()
+    {
+        (var user, _) = UserBuilder.Build();
+
+        var request = RequestRecipeJsonBuilder.Build();
+
+        var useCase = CreateUseCase(user);
+
+        Func<Task> act = async () => await useCase.Execute(recipeId: 1000, request);
+
+        (await act.Should().ThrowAsync<NotFoundException>())
+            .Where(e => e.Message.Equals(ResourceMessagesException.RECIPE_NOT_FOUND));
+    }
+
+    [Fact]
+    public async Task Error_Title_Empty()
+    {
+        (var user, _) = UserBuilder.Build();
+        var recipe = RecipeBuilder.Build(user);
+        var request = RequestRecipeJsonBuilder.Build();
+        request.Title = string.Empty;
+
+        var useCase = CreateUseCase(user, recipe);
+
+        Func<Task> act = async () => await useCase.Execute(recipe.Id, request);
+
+        (await act.Should().ThrowAsync<ErrorOnValidationException>())
+            .Where(e => e.ErrorMessages.Count == 1 &&
+                e.ErrorMessages.Contains(ResourceMessagesException.RECIPE_TITLE_EMPTY));
+    }
+
+    private static UpdateRecipeUseCase CreateUseCase(
+        MyRecipeBook.Domain.Entities.User user,
+        MyRecipeBook.Domain.Entities.Recipe? recipe = null)
+    {
+        var mapper = MapperBuilder.Build();
+        var loggedUser = LoggedUserBuilder.Build(user);
+        var unitOfWork = UnitOfWorkBuilder.Build();
+        var repository = new RecipeUpdateOnlyRepositoryBuilder().GetById(user, recipe).Build();
+
+        return new UpdateRecipeUseCase(loggedUser, unitOfWork, mapper, repository);
+    }
+}
---
+++ tests/WebApi.Test/Recipe/Update/UpdateRecipeTest.cs
@@ -0,0 +1,60 @@
+﻿using CommonTestUtilities.Requests;
+using CommonTestUtilities.Tokens;
+using FluentAssertions;
+using MyRecipeBook.Exceptions;
+using System.Globalization;
+using System.Net;
+using System.Text.Json;
+using WebApi.Test.InlineData;
+using Xunit;
+
+namespace WebApi.Test.Recipe.Update;
+public class UpdateRecipeTest : MyRecipeBookClassFixture
+{
+    private const string METHOD = "recipe";
+
+    private readonly Guid _userIdentifier;
+    private readonly string _recipeId;
+
+    public UpdateRecipeTest(CustomWebApplicationFactory factory) : base(factory)
+    {
+        _userIdentifier = factory.GetUserIdentifier();
+        _recipeId = factory.GetRecipeId();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var request = RequestRecipeJsonBuilder.Build();
+
+        var token = JwtTokenGeneratorBuilder.Build().Generate(_userIdentifier);
+
+        var response = await DoPut($"{METHOD}/{_recipeId}", request, token);
+
+        response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+
+    [Theory]
+    [ClassData(typeof(CultureInlineDataTest))]
+    public async Task Error_Title_Empty(string culture)
+    {
+        var request = RequestRecipeJsonBuilder.Build();
+        request.Title = string.Empty;
+
+        var token = JwtTokenGeneratorBuilder.Build().Generate(_userIdentifier);
+
+        var response = await DoPut($"{METHOD}/{_recipeId}", request, token, culture);
+
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
+
+        var expectedMessage = ResourceMessagesException.ResourceManager.GetString("RECIPE_TITLE_EMPTY", new CultureInfo(culture));
+
+        errors.Should().HaveCount(1).And.Contain(c => c.GetString()!.Equals(expectedMessage));
+    }
+}
