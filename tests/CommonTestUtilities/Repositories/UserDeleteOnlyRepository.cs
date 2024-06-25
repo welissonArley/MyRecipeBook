f9cb502eb1320715bf4a377fca0dc1afde01bb69
Welisson Arley <welissonarleyvs@gmail.com>
2024-06-25 08:35:20 +0100

    tests: adding unit tests for delete user account

---
+++ tests/CommonTestUtilities/Repositories/UserDeleteOnlyRepository.cs
@@ -0,0 +1,13 @@
+﻿using Moq;
+using MyRecipeBook.Domain.Repositories.User;
+
+namespace CommonTestUtilities.Repositories;
+public class UserDeleteOnlyRepository
+{
+    public static IUserDeleteOnlyRepository Build()
+    {
+        var mock = new Mock<IUserDeleteOnlyRepository>();
+
+        return mock.Object;
+    }
+}
---
+++ tests/UseCases.Test/User/Delete/Delete/DeleteUserAccountUseCaseTest.cs
@@ -0,0 +1,31 @@
+﻿using CommonTestUtilities.BlobStorage;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.Repositories;
+using FluentAssertions;
+using MyRecipeBook.Application.UseCases.User.Delete.Delete;
+using Xunit;
+
+namespace UseCases.Test.User.Delete.Delete;
+public class DeleteUserAccountUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        (var user, _) = UserBuilder.Build();
+
+        var useCase = CreateUseCase();
+
+        var act = async () => await useCase.Execute(user.UserIdentifier);
+
+        await act.Should().NotThrowAsync();
+    }
+
+    private static DeleteUserAccountUseCase CreateUseCase()
+    {
+        var unitOfWork = UnitOfWorkBuilder.Build();
+        var blobStorageService = new BlobStorageServiceBuilder().Build();
+        var repository = UserDeleteOnlyRepository.Build();
+
+        return new DeleteUserAccountUseCase(repository, blobStorageService, unitOfWork);
+    }
+}
