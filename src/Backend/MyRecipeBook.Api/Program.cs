f74e0b48b571047f6fb389a8713ef76d63f4fd48
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2026-03-03 19:37:25 -0500

    first commit

---
+++ src/Backend/MyRecipeBook.Api/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
