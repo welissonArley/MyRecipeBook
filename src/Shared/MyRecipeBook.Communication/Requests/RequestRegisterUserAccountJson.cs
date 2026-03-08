81d7c43e273d02d55ac0483f5b56043bada3d3d3
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2026-03-08 15:19:00 -0400

    feat: adding endpoint for register user account

---
+++ src/Shared/MyRecipeBook.Communication/Requests/RequestRegisterUserAccountJson.cs
@@ -0,0 +1,8 @@
+﻿namespace MyRecipeBook.Communication.Requests;
+
+public class RequestRegisterUserAccountJson
+{
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
