6f5dd5ff0acfa3742400703b29d63e4a43368fa5
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2026-03-15 17:10:29 -0400

    feat: adding culture support

--- src/Backend/MyRecipeBook.Api/Program.cs
+++ src/Backend/MyRecipeBook.Api/Program.cs
@@ -1,10 +1,29 @@
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
 
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportedCultures = new List<CultureInfo> { new("en"), new("pt-BR"), new("es") };
+
+    options.DefaultRequestCulture = new RequestCulture("en");
+
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+
+    options.RequestCultureProviders = [ new AcceptLanguageHeaderRequestCultureProvider() ];
+});
+
 var app = builder.Build();
+
+var localizationOptions = app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>();
+app.UseRequestLocalization(localizationOptions.Value);
 
 if (app.Environment.IsDevelopment())
 {
