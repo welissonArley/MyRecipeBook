63cf5362183620a49d19bdc9e89576d7c24a8a61
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2026-03-14 21:50:16 -0400

    feat: adding resource error messages file

--- src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserAccountValidator.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserAccountValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Exception;
 
 namespace MyRecipeBook.Application.UseCases.User.Register;
 
@@ -7,12 +8,12 @@ public class RegisterUserAccountValidator : AbstractValidator<RequestRegisterUse
 {
     public RegisterUserAccountValidator()
     {
-        RuleFor(user => user.Name).NotEmpty().WithMessage("O nome nao pode ser vazio");
-        RuleFor(user => user.Email).NotEmpty().WithMessage("O email nao pode ser vazio");
-        RuleFor(user => user.Password).NotEmpty().WithMessage("A senha nao pode ser vazia");
+        RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceMessagesException.VALIDATION_NAME_REQUIRED);
+        RuleFor(user => user.Email).NotEmpty().WithMessage(ResourceMessagesException.VALIDATION_EMAIL_REQUIRED);
+        RuleFor(user => user.Password).NotEmpty().WithMessage(ResourceMessagesException.VALIDATION_PASSWORD_REQUIRED);
         When(user => string.IsNullOrWhiteSpace(user.Email) == false, () =>
         {
-            RuleFor(user => user.Email).EmailAddress().WithMessage("O email deve ser valido");
+            RuleFor(user => user.Email).EmailAddress().WithMessage(ResourceMessagesException.VALIDATION_EMAIL_INVALID);
         });
     }
 }
