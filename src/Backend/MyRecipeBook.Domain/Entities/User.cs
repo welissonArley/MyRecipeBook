dc8fdf9f71c02375b9fe532c1f64d89af7d86240
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2026-03-20 16:35:41 -0400

    feat: adding entity user

---
+++ src/Backend/MyRecipeBook.Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace MyRecipeBook.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; private set; } = Guid.CreateVersion7();
+    public bool Active { get; set; } = true;
+
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
