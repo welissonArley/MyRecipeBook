27e2d3ffd855cc9005d8d97b6464038679e703b7
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2026-03-21 09:27:14 -0400

    feat: maping request in user entity

--- src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserAccountUseCase.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserAccountUseCase.cs
@@ -1,4 +1,5 @@
-﻿using MyRecipeBook.Communication.Requests;
+﻿using Mapster;
+using MyRecipeBook.Communication.Requests;
 using MyRecipeBook.Exception.ExceptionsBase;
 
 namespace MyRecipeBook.Application.UseCases.User.Register;
@@ -6,6 +7,13 @@ namespace MyRecipeBook.Application.UseCases.User.Register;
 public class RegisterUserAccountUseCase
 {
     public void Execute(RequestRegisterUserAccountJson request)
+    {
+        ValidateAndThrowOnFailures(request);
+
+        var user = request.Adapt<Domain.Entities.User>();
+    }
+
+    private void ValidateAndThrowOnFailures(RequestRegisterUserAccountJson request)
     {
         var validator = new RegisterUserAccountValidator();
 
