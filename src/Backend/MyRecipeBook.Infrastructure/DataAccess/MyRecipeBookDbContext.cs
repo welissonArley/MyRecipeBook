0ba40d076a4363b110d890ba842e8fe8aa9067d4
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2026-03-28 14:48:01 -0400

    feat: adding entity framework and dbcontext

---
+++ src/Backend/MyRecipeBook.Infrastructure/DataAccess/MyRecipeBookDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyRecipeBook.Domain.Entities;
+
+namespace MyRecipeBook.Infrastructure.DataAccess;
+
+internal class MyRecipeBookDbContext : DbContext
+{
+    public MyRecipeBookDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+
+    public DbSet<User> Users {  get; set; }
+}
