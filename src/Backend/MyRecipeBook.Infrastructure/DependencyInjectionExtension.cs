af3d87cb24515ebfe198488404b0fe745e7d7154
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2026-04-01 08:09:20 -0400

    feat: adding repository and dbcontext into DI

--- src/Backend/MyRecipeBook.Infrastructure/DependencyInjectionExtension.cs
+++ src/Backend/MyRecipeBook.Infrastructure/DependencyInjectionExtension.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MyRecipeBook.Domain.Repositories.User;
 using MyRecipeBook.Domain.Security.PasswordHashing;
+using MyRecipeBook.Infrastructure.DataAccess;
+using MyRecipeBook.Infrastructure.DataAccess.Repositories;
 using MyRecipeBook.Infrastructure.Security.PasswordHashing;
 
 namespace MyRecipeBook.Infrastructure;
@@ -11,6 +15,13 @@ public static class DependencyInjectionExtension
         public void AddInfrastructure()
         {
             services.AddScoped<IPasswordHasher, Argon2PasswordHasher>();
+
+            services.AddScoped<IUserWriteOnlyRepository, UserRepository>();
+
+            services.AddDbContext<MyRecipeBookDbContext>(config =>
+            {
+                config.UseMySQL("Server=localhost;Database=meulivrodereceitas;Uid=root;Pwd=@Password123;");
+            });
         }
     }
 }
