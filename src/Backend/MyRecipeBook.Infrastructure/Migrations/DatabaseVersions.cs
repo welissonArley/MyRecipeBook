8abcc7ace014cdd4bfbf7c0185915375c2dacabe
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2026-04-06 06:23:18 -0400

    feat: migration with FluentMigrator

---
+++ src/Backend/MyRecipeBook.Infrastructure/Migrations/DatabaseVersions.cs
@@ -0,0 +1,6 @@
+﻿namespace MyRecipeBook.Infrastructure.Migrations;
+
+internal abstract class DatabaseVersions
+{
+    internal const int TABLE_USERS = 1;
+}
---
+++ src/Backend/MyRecipeBook.Infrastructure/Migrations/Versions/Version0000001.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator;
+
+namespace MyRecipeBook.Infrastructure.Migrations.Versions;
+
+[Migration(DatabaseVersions.TABLE_USERS, "Creating Users table")]
+internal class Version0000001 : ForwardOnlyMigration
+{
+    public override void Up()
+    {
+        Create.Table("Users")
+            .WithColumn("Id").AsGuid().PrimaryKey().NotNullable()
+            .WithColumn("Active").AsBoolean().NotNullable().WithDefaultValue(true)
+            .WithColumn("Name").AsString(250).NotNullable()
+            .WithColumn("Email").AsString(250).NotNullable()
+            .WithColumn("Password").AsString(2000).NotNullable();
+    }
+}
