0e84b049557b7fa9651d9940cb94e7d15ea8cd8b
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2026-04-09 13:42:18 -0400

    feat: implementing our first unit test

--- tests/Validators.Tests/User/Register/RegisterUserAccountValidatorTests.cs
+++ tests/Validators.Tests/User/Register/RegisterUserAccountValidatorTests.cs
@@ -1,6 +1,31 @@
-﻿namespace Validators.Tests.User.Register;
+﻿using MyRecipeBook.Application.UseCases.User.Register;
+using MyRecipeBook.Communication.Requests;
+
+namespace Validators.Tests.User.Register;
 
 public class RegisterUserAccountValidatorTests
 {
+    [Fact]
+    public void Success()
+    {
+        //AAA
 
+        // Arrange
+
+        var request = new RequestRegisterUserAccountJson
+        {
+            Name = "Welisson",
+            Email = "welisson@gmail.com",
+            Password = "123456789"
+        };
+
+        var validator = new RegisterUserAccountValidator();
+
+        // Act
+
+        var result = validator.Validate(request);
+
+        // Assert
+        Assert.True(result.IsValid);
+    }
 }
