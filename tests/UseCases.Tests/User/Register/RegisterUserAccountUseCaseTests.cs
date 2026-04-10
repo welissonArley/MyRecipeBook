e52bef9d5acd7fe18d634990f85799a961010eb1
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2026-04-10 15:01:10 -0400

    feat: starting unit tests for register account usecase

---
+++ tests/UseCases.Tests/User/Register/RegisterUserAccountUseCaseTests.cs
@@ -0,0 +1,20 @@
+﻿using CommonTestUtilities.Requests;
+using MyRecipeBook.Application.UseCases.User.Register;
+
+namespace UseCases.Tests.User.Register;
+
+public class RegisterUserAccountUseCaseTests
+{
+    [Fact]
+    public async Task Success()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+
+        var useCase = CreateUseCase();
+    }
+
+    private RegisterUserAccountUseCase CreateUseCase()
+    {
+        return new RegisterUserAccountUseCase(null, null, null, null);
+    }
+}
