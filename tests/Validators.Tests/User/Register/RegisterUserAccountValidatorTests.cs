e3b8ea5034dbd3443194b21fe03cee1df0bebfa7
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2026-04-10 11:10:48 -0400

    feat: testing requests with invalid values for validator

--- tests/Validators.Tests/User/Register/RegisterUserAccountValidatorTests.cs
+++ tests/Validators.Tests/User/Register/RegisterUserAccountValidatorTests.cs
@@ -1,5 +1,6 @@
 ﻿using CommonTestUtilities.Requests;
 using MyRecipeBook.Application.UseCases.User.Register;
+using MyRecipeBook.Exception;
 using Shouldly;
 
 namespace Validators.Tests.User.Register;
@@ -19,5 +20,77 @@ public class RegisterUserAccountValidatorTests
 
         // Assert
         result.IsValid.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_WhenNameIsEmpty()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+        request.Name = string.Empty;
+
+        var validator = new RegisterUserAccountValidator();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(error => error.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_NAME_REQUIRED));
+        });
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_WhenEmailIsEmpty()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+        request.Email = string.Empty;
+
+        var validator = new RegisterUserAccountValidator();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(error => error.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_EMAIL_REQUIRED));
+        });
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_WhenPasswordIsEmpty()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+        request.Password = string.Empty;
+
+        var validator = new RegisterUserAccountValidator();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(error => error.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_PASSWORD_REQUIRED));
+        });
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_WhenEmailIsInvalid()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+        request.Email = "we.com";
+
+        var validator = new RegisterUserAccountValidator();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(error => error.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_EMAIL_INVALID));
+        });
     }
 }
