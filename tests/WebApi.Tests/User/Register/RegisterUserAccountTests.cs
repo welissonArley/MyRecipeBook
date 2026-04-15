4c759813df7f6964d4fb1e155dc8a01937cec6d3
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2026-04-14 20:14:44 -0400

    feat: adding factory for our integration tests

--- tests/WebApi.Tests/User/Register/RegisterUserAccountTests.cs
+++ tests/WebApi.Tests/User/Register/RegisterUserAccountTests.cs
@@ -1,17 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using CommonTestUtilities.Requests;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http.Json;
 
 namespace WebApi.Tests.User.Register;
 
 public class RegisterUserAccountTests : IClassFixture<WebApplicationFactory<Program>>
 {
-    public RegisterUserAccountTests()
+    private readonly HttpClient _httpClient;
+
+    public RegisterUserAccountTests(WebApplicationFactory<Program> factory)
     {
-        
+        _httpClient = factory.CreateClient();
     }
 
     [Fact]
     public async Task Success()
     {
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
 
+        await _httpClient.PostAsJsonAsync("/users", request);
+    }
+
+    [Fact]
+    public async Task Validate_ShouldBeAnErrorResponse_WhenNameIsEmpty()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+        request.Name = string.Empty;
+
+        await _httpClient.PostAsJsonAsync("/users", request);
     }
 }
