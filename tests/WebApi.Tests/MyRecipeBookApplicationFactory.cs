b611aeed95e4a6b661e06f3d3b6c23504975c927
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2026-04-19 10:55:16 -0400

    tests: getting connection string for database with testcontainer

--- tests/WebApi.Tests/MyRecipeBookApplicationFactory.cs
+++ tests/WebApi.Tests/MyRecipeBookApplicationFactory.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
 using Testcontainers.MySql;
 
 namespace WebApi.Tests;
@@ -17,7 +18,16 @@ public class MyRecipeBookApplicationFactory : WebApplicationFactory<Program>, IA
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        builder.UseEnvironment("Tests");
+        builder.UseEnvironment("Tests")
+            .ConfigureAppConfiguration((_, configuration) =>
+            {
+                var parameters = new Dictionary<string, string?>
+                {
+                    ["ConnectionStrings:DbConnection"] = _mySqlContainer.GetConnectionString()
+                };
+
+                configuration.AddInMemoryCollection(parameters);
+            });
     }
 
     public async Task InitializeAsync()
