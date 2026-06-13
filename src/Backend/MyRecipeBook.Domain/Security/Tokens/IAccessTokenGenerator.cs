4786cc2248236571cabd81f7ad57d1fa0d6ddb46
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2026-06-13 14:16:20 -0400

    feat: adding access token jwt generator

---
+++ src/Backend/MyRecipeBook.Domain/Security/Tokens/IAccessTokenGenerator.cs
@@ -0,0 +1,8 @@
+﻿using MyRecipeBook.Domain.Entities;
+
+namespace MyRecipeBook.Domain.Security.Tokens;
+
+public interface IAccessTokenGenerator
+{
+    string Generate(User user);
+}
---
+++ src/Backend/MyRecipeBook.Infrastructure/Security/Tokens/Access/JwtTokenHandler.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using MyRecipeBook.Domain.Entities;
+using MyRecipeBook.Domain.Security.Tokens;
+using System.Security.Claims;
+using System.Text;
+
+namespace MyRecipeBook.Infrastructure.Security.Tokens.Access;
+
+internal sealed class JwtTokenHandler : IAccessTokenGenerator
+{
+    private readonly uint _expirationTimeMinutes;
+    private readonly string _signingKey;
+
+    public JwtTokenHandler(uint expirationTimeMinutes, string signingKey)
+    {
+        _expirationTimeMinutes = expirationTimeMinutes;
+        _signingKey = signingKey;
+    }
+
+    public string Generate(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new (JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+        };
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Expires = DateTime.UtcNow.AddMinutes(_expirationTimeMinutes),
+            SigningCredentials = new SigningCredentials(Credentials(), SecurityAlgorithms.HmacSha256),
+            Subject = new ClaimsIdentity(claims)
+        };
+
+        var handler = new JsonWebTokenHandler();
+
+        return handler.CreateToken(tokenDescriptor);
+    }
+
+    private SymmetricSecurityKey Credentials()
+    {
+        var keyBytes = Encoding.UTF8.GetBytes(_signingKey);
+
+        return new SymmetricSecurityKey(keyBytes);
+    }
+}
