309d9fa21f18e80d005c42f2cc9bd0ba0ba453d6
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2026-07-05 15:15:35 -0400

    feat: adding migration for recipes

--- src/Backend/MyRecipeBook.Infrastructure/Migrations/DatabaseVersions.cs
+++ src/Backend/MyRecipeBook.Infrastructure/Migrations/DatabaseVersions.cs
@@ -3,4 +3,5 @@
 internal abstract class DatabaseVersions
 {
     internal const int TABLE_USERS = 1;
+    internal const int TABLE_RECIPES = 2;
 }
---
+++ src/Backend/MyRecipeBook.Infrastructure/Migrations/Versions/Version0000002.cs
@@ -0,0 +1,41 @@
+using FluentMigrator;
+using System.Data;
+
+namespace MyRecipeBook.Infrastructure.Migrations.Versions;
+
+[Migration(DatabaseVersions.TABLE_RECIPES, "Creating Recipe tables")]
+public class Version0000002 : ForwardOnlyMigration
+{
+    public override void Up()
+    {
+        Create.Table("Recipes")
+            .WithColumn("Id").AsGuid().PrimaryKey().NotNullable()
+            .WithColumn("Active").AsBoolean().NotNullable().WithDefaultValue(true)
+            .WithColumn("Title").AsString(250).NotNullable()
+            .WithColumn("CookTime").AsInt32().NotNullable()
+            .WithColumn("UserId").AsGuid().NotNullable()
+                .ForeignKey("FK_Recipes_Users_UserId", "Users", "Id");
+
+        Create.Table("RecipeIngredients")
+            .WithColumn("Id").AsGuid().PrimaryKey().NotNullable()
+            .WithColumn("Active").AsBoolean().NotNullable().WithDefaultValue(true)
+            .WithColumn("Item").AsString(250).NotNullable()
+            .WithColumn("RecipeId").AsGuid().NotNullable()
+                .ForeignKey("FK_RecipeIngredients_Recipes_RecipeId", "Recipes", "Id").OnDelete(Rule.Cascade);
+
+        Create.Table("RecipeInstructions")
+            .WithColumn("Id").AsGuid().PrimaryKey().NotNullable()
+            .WithColumn("Active").AsBoolean().NotNullable().WithDefaultValue(true)
+            .WithColumn("Order").AsInt32().NotNullable()
+            .WithColumn("Description").AsString(2000).NotNullable()
+            .WithColumn("RecipeId").AsGuid().NotNullable()
+                .ForeignKey("FK_RecipeInstructions_Recipes_RecipeId", "Recipes", "Id").OnDelete(Rule.Cascade);
+
+        Create.Table("RecipeDishTypes")
+            .WithColumn("Id").AsGuid().PrimaryKey().NotNullable()
+            .WithColumn("Active").AsBoolean().NotNullable().WithDefaultValue(true)
+            .WithColumn("Type").AsInt32().NotNullable()
+            .WithColumn("RecipeId").AsGuid().NotNullable()
+                .ForeignKey("FK_RecipeDishTypes_Recipes_RecipeId", "Recipes", "Id").OnDelete(Rule.Cascade);
+    }
+}
